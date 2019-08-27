b040218eb962d274aa3bcbcd9f441936a30b8087
Zziaach <ch-zz@outlook.com>
2019-08-28 00:04:40 +0100

    add client

---
+++ src/main/resources/static/precache-manifest.9edfbee2772d59eadfd1e10c807f2bf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a7cf6ffe8a5f7c4cb9c6505ad95476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbbb0cb761ed7accc314",
+    "url": "/static/css/2.a4c599c5.chunk.css"
+  },
+  {
+    "revision": "d6742e7ad147d1110927",
+    "url": "/static/css/main.83754762.chunk.css"
+  },
+  {
+    "revision": "cbbb0cb761ed7accc314",
+    "url": "/static/js/2.59924f61.chunk.js"
+  },
+  {
+    "revision": "d6742e7ad147d1110927",
+    "url": "/static/js/main.97004fb4.chunk.js"
+  },
+  {
+    "revision": "910fcab45efa6475fe2b",
+    "url": "/static/js/runtime~main.8f9300fe.js"
+  },
+  {
+    "revision": "6e7be65f1835cd8830cb812e5f5cdac7",
+    "url": "/static/media/note.6e7be65f.svg"
+  }
+]);
